c5af1cbac7e4e138a55ab34e3884040e89b97b9c
Ferrum <105937347+Mexekanec@users.noreply.github.com>
2025-10-05 16:10:54 +0300

    Add files via upload

---
+++ Cods/Interface/IComponentNamingService.cs
@@ -0,0 +1,16 @@
+﻿using Laboratorio.Cods.Circut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio.Cods.Interface
+{
+    public interface IComponentNamingService
+    {
+        string GetNextName(ComponentType type);
+        void ResetCounter(ComponentType type);
+        void ComponentDeleted(ComponentType type, string name);
+    }
+}
---
+++ Cods/Interface/IComponentSettingsService.cs
@@ -0,0 +1,21 @@
+﻿using Laboratorio.Cods.Circut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio.Cods.Interface
+{
+    public interface IComponentSettingsService
+    {
+        ComponentType SelectedComponentType { get; set; }
+        double ResistanceValue { get; set; }
+        double CapacitanceValue { get; set; }
+        double VoltageValue { get; set; }
+        double InductanceValue { get; set; }
+        PlacementDirection PlacementDirection { get; set; }
+
+        event EventHandler SettingsChanged;
+    }
+}
